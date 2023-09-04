05b633e8157106318c157c689f7d7694228a7d25
Stephd91 <75246581+Stephd91@users.noreply.github.com>
2023-09-04 10:02:40 +0200

    Add files via upload

---
+++ PostgreSQL_backup.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+my_password="MTQ4NTktc3RlcGhh"
+date=$(date +%Y%m%d)
+rootbackupdirectory="/tmp/mysqldumps/"
+backupdirectory="/tmp/mysqldumps/$date"
+backup_file="$date-all-databases-backup.sql"
+
+if [ ! -d $rootbackupdirectory ] ; then
+    mkdir $rootbackupdirectory
+fi
+
+if [ ! -d $backupdirectory ] ; then
+    mkdir $backupdirectory
+fi
+
+if mysqldump --all-databases --user=root --password="$my_password" > "$backupdirectory/$backup_file" ; then
+    echo 'SQL dump created'
+fi
+
+
+
